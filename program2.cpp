37d56dbfd63f0dbe2d1b4d41f2cc071517c5afab
Kanak Narware <kanaknarware3110@gmail.com>
2026-08-04 17:02:38 +0000

    Add this file

---
+++ program2.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main() {
+    char letter = 'K';
+    cout << "ASCII value of " << letter << " is " << static_cast<int>(letter) << endl; 
+    cout << "ASCII value of 'a' is " << (int)'a' << endl; 
+    return 0;
+}
