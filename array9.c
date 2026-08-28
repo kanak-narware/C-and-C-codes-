9413ade498f7356734e1f7dc7ee5e038ceca57e8
Kanak Narware <kanaknarware3110@gmail.com>
2026-08-28 16:35:14 +0000

    add

---
+++ array9.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int sumArrayRecursive(int arr[], int n) {
+    if (n <= 0) {
+        return 0;
+    }
+    return arr[n - 1] + sumArrayRecursive(arr, n - 1);
+}
+
+int main() {
+    int arr[] = {5, 2, 9, 1, 7};
+    int size = sizeof(arr) / sizeof(arr[0]);    
+    int totalSum = sumArrayRecursive(arr, size);    
+    printf("Sum of array elements: %d\n", totalSum);
+    
+    return 0;
+}
